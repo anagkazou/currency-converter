a87a1297e29669746b844559286e986444934a6d
david <thedavidomole@gmail.com>
2020-03-03 15:23:14 +0100

    Add Currency array for select tags

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-let demo = () => {
-  let rate = fx(1)
-    .from("GBP")
-    .to("USD");
-  alert("£1 = $" + rate.toFixed(4));
-};
-
+// let demo = () => {
+//   let rate = fx(1)
+//     .from("GBP")
+//     .to("USD");
+//   alert("£1 = $" + rate.toFixed(4));
+// };
 const fixer =
   "http://data.fixer.io/api/latest?access_key=5b2d17c9e871b87d91f00c86244fc91d";
 
-fetch(fixer)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    data = json;
-    console.log(data);
-  });
+// !(async function() {
+//   let data = await fetch(fixer)
+//     .then(response => response.json())
+//     .then(data => populateSelect(data))
+//     .catch(error => console.error(error));
+// })();
+(function populateSelect() {
+  var x;
+  for (x in myData) console.log(myData[x].name);
+})();
+//console.log(data);
